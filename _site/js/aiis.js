5890e2fc265548d6746da6732f3da97faca62b36
delilah1004 <pskdy8414@naver.com>
2020-12-18 11:16:46 +0900

    nav.html scripts 코드 aiis.js 로 이동

--- _site/js/aiis.js
+++ _site/js/aiis.js
@@ -1,13 +1,18 @@
-$(document).ready(function(){
-
-	
-
-})
-
 $(function(){
   // 기존 css에서 플로팅 배너 위치(top)값을 가져와 저장한다.
 	var floatPosition = parseInt($("#floatMenu").css('top'));
 	// 250px 이런식으로 가져오므로 여기서 숫자만 가져온다. parseInt( 값 );
+
+	// Navigation Menu
+	var navLinks = document.querySelectorAll('#mainNav .nav-link');
+	// Navigation 펼치기/접기 버튼
+	var toggleButton = document.querySelector('#mainNav .navbar-toggler');
+
+	navLinks.forEach(function(el){
+		el.addEventListener('click',function(){
+			toggleButton.click();
+		}, false);
+	})
 
 	$(window).scroll(function() {
 		// 현재 스크롤 위치를 가져온다.
